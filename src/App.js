ea43dbbe4fea94be8851a4d33f43fd7b297456cc
Francisco Berti da Cruz <fbc991@live.com>
2023-03-31 16:07:45 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Home } from './pages/Home';
-import { Profile } from './pages/Profile';
-import { Contact } from './pages/Contact';
-import { Navbar } from './Navbar';
+
 import { createContext } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
@@ -14,17 +10,7 @@ function App() {
 
   return (
     <div className="App">
-      <QueryClientProvider client={client}>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/profile' element={<Profile />} />
-            <Route path='/contact' element={<Contact />} />
-            <Route path='/*' element={<h1>PAGE NOT FOUND</h1>} />
-          </Routes>
-        </Router>
-      </QueryClientProvider>
+
     </div >
   );
 }
