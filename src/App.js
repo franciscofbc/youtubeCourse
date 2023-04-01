db964153215aa9cc11a9f2913dbdf5fa05a5869f
Francisco Berti da Cruz <fbc991@live.com>
2023-03-31 21:16:44 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,61 +1,14 @@
-import { useState } from 'react';
 import './App.css';
-import { Task } from './Task';
+import { Form } from './components/Form';
 
 function App() {
-  const [todoList, setTodoList] = useState([])
-  const [newTask, setNewTask] = useState('')
 
-  const handleChange = (event) => {
-    setNewTask(event.target.value)
-  }
 
-  const addTask = () => {
-    const task = {
-      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
-      taskName: newTask,
-      completed: false
-    }
-    setTodoList([...todoList, task])
-
-  }
-
-  const deleteTask = (id) => {
-    setTodoList(todoList.filter(task => task.id !== id))
-  }
-
-  const completeTask = (id) => {
-    setTodoList(todoList.map(task => {
-      if (task.id === id) {
-        return { ...task, completed: true }
-      } else {
-        return task
-      }
-    }))
-  }
 
   return (
+
     <div className="App">
-
-      <div className='addTask'>
-        <input placeholder='Type your task' onChange={handleChange} />
-        <button onClick={addTask}>Add Task</button>
-      </div>
-
-      <div className='list'>
-        {todoList.map((task) => {
-          return (
-            <Task
-              taskName={task.taskName}
-              id={task.id}
-              completed={task.completed}
-              deleteTask={deleteTask}
-              completeTask={completeTask}
-            />
-          )
-        })}
-
-      </div>
+      <Form />
 
     </div >
   );
